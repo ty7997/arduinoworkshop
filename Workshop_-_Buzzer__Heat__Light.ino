6391240774a92ded78e3cb0cf35c44e8c9b90a28
ty7997 <61234839+ty7997@users.noreply.github.com>
2020-02-19 15:58:23 +0000

    Uploaded Workshop Code

---
+++ Workshop_-_Buzzer__Heat__Light.ino
@@ -0,0 +1,57 @@
+void setup() {
+  // put your setup code here, to run once:
+ // pinMode(4, OUTPUT); //LED connected to pin 4 so set as output
+  pinMode(4, OUTPUT); //buzzer connected to pin 4 so set as output
+  pinMode(A0, INPUT); //heat sensor connected to A0 so set as input
+  pinMode(A1, INPUT); //heat sensor connected to A0 so set as input
+  Serial.begin(9600); //this is used to print out values
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int rawvoltage = analogRead(A0); //get the value from the sensor connected to pin A0
+  int light = analogRead(A1);
+  float celsius = ((rawvoltage/1024.0) * 5000 / 10); //converts analog signal to temperature
+  Serial.println(celsius); //print out celsius to see what the temp is
+Serial.println(light); //print out elsius to 
+
+  if(light > 600){
+
+    digitalWrite(6, HIGH); //turn LED connected to pin 4 on
+    delay(2000); //wait for 2000 milliseconds
+  }
+  else if(light <= 600){
+    
+    digitalWrite(6,LOW); //turn LED connected to pin 4 off
+    delay(2000); //wait for 2000 milliseconds
+    
+  }
+  
+  if(celsius > 24){
+    
+    //digitalWrite(4, HIGH); //turn LED connected to pin 4 on
+    tone(4, 600);
+    delay(2000); //wait for 2000 milliseconds
+    
+  }else if(celsius <= 24){
+    
+    digitalWrite(4,LOW); //turn LED connected to pin 4 off
+    tone(4, 200);
+    delay(2000); //wait for 2000 milliseconds
+    
+  }
+
+    if(celsius > 24){
+    
+    digitalWrite(4, HIGH); //turn LED connected to pin 4 on
+    delay(2000); //wait for 2000 milliseconds
+    
+  }else if(celsius <= 24){
+    
+    digitalWrite(4,LOW); //turn LED connected to pin 4 off
+    delay(2000); //wait for 2000 milliseconds
+    
+  }
+   
+}
